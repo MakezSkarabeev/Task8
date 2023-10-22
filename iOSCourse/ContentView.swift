9f02cabfa8977c6fa2ddb9c236a1eefd82106dcc
msivenkov <msivenkov@rencredit.ru>
2023-10-22 15:24:48 +0300

    TASK8 - added volume slider

---
+++ iOSCourse/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  iOSCourse
+//
+//  Created by Sivenkov maxim  on 01.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var sliderHeight: CGFloat = 180
+
+    @State var progress: Double = 0
+    @State var lastDragValue: Double = 0
+    @State var heightScale: Double = 1
+    @State var widthScale: Double = 1
+    @State var dragSide: UnitPoint = .zero
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.black)
+                .ignoresSafeArea()
+
+            RoundedRectangle(cornerRadius: 18.0)
+                .fill(.ultraThinMaterial)
+                .overlay(alignment: .bottom) {
+                    Rectangle()
+                        .fill(.blue)
+                        .scaleEffect(y: progress, anchor: .bottom)
+                        .clipShape(RoundedRectangle(cornerRadius: 18.0))
+                }
+                .gesture(
+                    DragGesture(coordinateSpace: .global)
+                        .onChanged { value in
+                            let newValue = -value.translation.height / sliderHeight + lastDragValue
+
+                            switch newValue {
+                            case _ where newValue > 1:
+                                progress = 1
+                                heightScale = pow(newValue, 1/8)
+                                widthScale = 1 / pow(newValue, 1/10)
+                                dragSide = .bottom
+
+                            case _ where newValue < 0:
+                                progress = 0
+                                heightScale = pow(1 - newValue, 1/8)
+                                widthScale = 1 / pow(1 - newValue, 1/10)
+                                dragSide = .top
+
+                            default:
+                                progress = newValue
+                            }
+                            print(newValue)
+                        }
+                        .onEnded({ _ in
+                            lastDragValue = progress
+                            heightScale = 1
+                            widthScale = 1
+                        })
+
+                )
+                .frame(width: 80, height: sliderHeight)
+                .scaleEffect(
+                    x: widthScale,
+                    y: heightScale,
+                    anchor: dragSide
+                )
+                .animation(.default, value: heightScale)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
